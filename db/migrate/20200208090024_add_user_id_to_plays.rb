3d2fe129c14821ba7caff08a60819dde1ade7106
Abduraimov Muminjon <muminjonguru@outlook.com>
2020-02-08 14:02:46 +0500

    navigation and db associations

---
+++ db/migrate/20200208090024_add_user_id_to_plays.rb
@@ -0,0 +1,5 @@
+class AddUserIdToPlays < ActiveRecord::Migration[6.0]
+  def change
+    add_column :plays, :user_id, :integer
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_07_174638) do
+ActiveRecord::Schema.define(version: 2020_02_08_090024) do
 
   create_table "plays", force: :cascade do |t|
     t.string "title"
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 2020_02_07_174638) do
     t.string "director"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
